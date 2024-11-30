cd64d29a72ec0cc9ea46e67cb2ed03aa6581b961
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-11-30 20:01:27 +0800

    feat: add get appointments integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import AppointmentForm from './components/AppointmentForm';
 import AppointmentsTable from './components/AppointmentsTable';
 import { Box, Typography } from '@mui/material';
+import { getAppointments } from './api/api';
+
+interface AppointmentTypes {
+  patient_name: string;
+  appointment_start_date: Date;
+  appointment_start_time: Date;
+  appointment_end_date: Date;
+  appointment_end_time: Date;
+  comments: string;
+}
 
 function App() {
+  const [appointments, setAppointments] = useState<AppointmentTypes[]>([])
+
+  useEffect(() => {
+    const fetchAppointments = async() => {
+      try {
+        const response = await getAppointments()
+        setAppointments(response)
+        
+      } catch(error) {
+        if (error instanceof Error) {
+          alert(error.message)
+        }
+      }
+    }
+
+    fetchAppointments()
+  }, [])
+
   return (
     <Box className="app">
       <Box className="app-header">
@@ -12,7 +41,7 @@ function App() {
         </Typography>
         <Box className="app-body">
           <Box className="app-box">
-            <AppointmentsTable />
+            <AppointmentsTable rows={appointments} />
           </Box>
           <Box className="app-box">
             <AppointmentForm />
