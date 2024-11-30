791fc064e1d8d88be5f0a089ba1466be51e8a222
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-11-30 15:36:42 +0800

    feat: add initial api functions and integrate create appointment

---
+++ src/api/api.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+export const getAppointments = async () => {
+  try {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/`);
+    return response.data;
+  } catch(error) {
+    console.error(error);
+    return error.message
+  }
+}
+
+export const createAppointment = async (data) => {
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/appointment/create`, data);
+    return {
+      status: response.status,
+      message: response.data.message
+    };
+  } catch(error) {
+    console.error(error);
+    throw new Error(error.response.data.error)
+  }
+}
+
+export const editAppointment = async (data) => {
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/edit`, data);
+    return response.data;
+  } catch(error) {
+    console.error(error);
+    return error.message
+  }
+}
+
+export const deleteAppointment = async (data) => {
+  try {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/delete`, data);
+    return response.data;
+  } catch(error) {
+    console.error(error);
+    return error.message
+  }
+}
