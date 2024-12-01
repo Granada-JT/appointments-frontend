61bca129610bd7ae5a4b354e2a25ac7145d3dfcb
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-12-02 04:27:17 +0800

    fix: change eslint config to typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,48 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import react from "eslint-plugin-react";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
 export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.node,
+        localStorage: true,
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: react,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/jsx-uses-vars": "error",
+      "no-undef": "off",
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+          allowSeparatedGroups: false,
+        },
+      ],
+    },
+  },
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
